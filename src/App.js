7039175271741065c7536661b044db2e59d02eeb
vaibhav <arora1234vaibhav@gmail.com>
2021-09-01 16:05:43 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import from React from "react";
+import React from 'react';
+// import Board from './component/Board';
 
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+const App = () => {
+  return (
+    <div>
+      <h1>TIC TAC TOE</h1>
+  
+    </div>
+  );
+};
+
+export default App;
